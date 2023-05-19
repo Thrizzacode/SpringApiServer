198ddbc22e1149c6880819ea20381cb31c6bf9c8
ApiServer <micky840814@gmail.com>
2023-05-19 21:47:23 +0800

    1.modify memberController

--- src/main/java/server/api/controller/MemberController.java
+++ src/main/java/server/api/controller/MemberController.java
@@ -1,6 +1,7 @@
 package server.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.api.model.Member;
@@ -17,6 +18,7 @@ public class MemberController {
     MemberService memberService;
 
     @RequestMapping("/members")
+    @CrossOrigin("*")
     public List<Member> getAllMembers() {
         return memberService.getAllMembers();
     }
